fc5e34a45944d96c0a2ed25f11bb96787d291676
Souten <k.kenshirou@gmail.com>
2026-07-24 12:09:18 +0900

    fix(steamos): recover owned sessions before display init

--- src/main.cpp
+++ src/main.cpp
@@ -258,8 +258,8 @@ int main(int argc, char *argv[]) {
   // Adding guard here first as it also performs recovery after crash,
   // otherwise people could theoretically end up without display output.
   // It also should be destroyed before forced shutdown to expedite the cleanup.
-  auto display_device_deinit_guard = display_device::init(platf::appdata() / "display_device.state", config::video);
   steamos_virtual_session::cleanup_orphan_sessions();
+  auto display_device_deinit_guard = display_device::init(platf::appdata() / "display_device.state", config::video);
   if (!display_device_deinit_guard) {
     BOOST_LOG(error) << "Display device session failed to initialize"sv;
   }
